b686f142f250172cc8bd0b6349eac6654f5a61fd
Yejin <skyaiwn08@gmail.com>
2023-06-26 01:45:55 +0900

    Fix: 불필요한 코드 제거

--- src/components/DrawingBoard.js
+++ src/components/DrawingBoard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Canvas, Path } from "@shopify/react-native-skia";
 import {
